772843ff62314289ec39498314332d8b17a9cf35
Isaac Mulatya <imulatya30@gmail.com>
2026-07-02 21:54:26 +0300

    structural architecture and first component rendered

---
+++ my-framework-portfolio/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav style={{ backgroundColor: '#1e40af', padding: '15px', color: 'white' }}>
+      <strong>Frontend</strong>
+      <span style={{ float: 'right' }}>Click</span>
+    </nav>
+  );
+}
+
+export default Navbar;
